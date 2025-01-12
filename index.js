e09f1b9bbe7adc0b71e7140877f830789a7300fa
MarkUA <mark.d.nahorniak@gmail.com>
2025-01-12 13:01:05 +0200

    fix

--- index.js
+++ index.js
@@ -1,25 +1,17 @@
 // Task 1
-
-let ownerName = prompt("Введіть ваше ім'я:");
-let accountNumber = Number(prompt("Введіть номер карти 16 цифер :"));
-let balance = Number(prompt("Введіть початковий баланс рахунку:"));
-
-if(accountNumber.lenght < 16){
-} else {
-    "Ви ввели неправельний номер карти"
+// 0936391518358173
+const bankAccount = {
+    ownerName: "Mark", accountNumber: "0936391518358173",balance: 58703
 }
-
-const bankAccount = [ownerName, accountNumber, balance]
-
 let deposit = Number(prompt("Поповнення рахунку:"));
-bankAccount[2] += deposit
+bankAccount.balance += deposit
 
 let withdraw = Number(prompt("Зняти гроші:"));
 
-if(bankAccount[2] >= withdraw) {
-    bankAccount[2] -= withdraw;
-    alert("Ви зняни готівку")
-    alert(`У вас залишилось на разунку: ${bankAccount[2]}`)
+if(bankAccount.balance >= withdraw) {
+    bankAccount.balance -= withdraw;
+    alert("Ви зняли готівку")
+    alert(`У вас залишилось на разунку: ${bankAccount.balance}`)
 } else {
     alert("У вас не вистачає грошей щоб зняти гроші");
 }
@@ -27,27 +19,28 @@ if(bankAccount[2] >= withdraw) {
 console.log(bankAccount)
 
 // Task 2
+const weather = {
+    temperature: 3, humidity: "65%", windSpeed: "14m/s"
+}
 
-let temperature = Number(prompt("Ведіть температупу"))
-let humidity = "65%"
-let windSpeed = "14m/s"
-
-if (temperature < 0) {
+if (weather.temperature < 0) {
     console.log("Температура нижче 0 градусів Цельсія.");
 } else {
     console.log("Температура вище або рівна 0 градусів Цельсія.");
 }
 
 // Task 3
-const userLog = ["Mark", "beta@gmail.com", "Beta@321"]
+const userLog = {
+    name: "Mark", mail: "blogernoname21@gmail.com", password: "Mark@123"
+}
 
 let owName = prompt("Введіть ваше ім'я:");
 let accountMail = (prompt("Введіть свою пошту:"));
 let password = (prompt("Введіть пароль:"));
 
-const checkName = owName === userLog[0]
-const checkMail = accountMail === userLog[1]
-const checkPas = password === userLog[2]
+const checkName = owName === userLog.name
+const checkMail = accountMail === userLog.mail
+const checkPas = password === userLog.password
 
 if(checkName, checkMail, checkPas){
     alert("Ви війшли в акаунт")
@@ -56,10 +49,12 @@ alert("Повторіть спробу")
 }
 
 // Task 4
-let movie = ["Сам удома 1", 1990, 7.7];
+let movie = {
+    name: "Сам удома 1", year: 1990, rating: 7.7
+}
 
-if (movie[3] > 6) {
-  alert("Цей фільм хороший так як рейтинг вище за 6");
+if (movie.rating > 7) {
+  alert("Цей фільм хороший так як рейтинг вище за 7");
 } else {
   alert("Не рекомендую цей фільм так як він поганий");
 }
